fc25523e87128f16afe23e22b14b48bb7b4a64fd
Dewchandra <dewprakashchandra@gmail.com>
2022-02-08 00:59:34 +0530

    added

---
+++ msp.js
@@ -0,0 +1,14 @@
+ Volume in drive D is New Volume
+ Volume Serial Number is 7E04-C6BA
+
+ Directory of D:\New folder\GITHUB\MyProject
+
+08-02-2022  00:57    <DIR>          .
+08-02-2022  00:57    <DIR>          ..
+08-02-2022  00:29               120 git
+07-02-2022  23:37               389 index.html
+08-02-2022  00:57                 0 msp.js
+07-02-2022  23:37               358 p.php
+08-02-2022  00:29               436 text1.txt
+               5 File(s)          1,303 bytes
+               2 Dir(s)  30,713,339,904 bytes free
